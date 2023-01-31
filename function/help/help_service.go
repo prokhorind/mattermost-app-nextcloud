65990e45445927d560c274bca6e389be96ae83de
omelnychuk <ostapss222@gmail.com>
2023-01-31 22:48:59 +0200

    Help controller added

--- function/help/help_service.go
+++ function/help/help_service.go
@@ -1,13 +1,10 @@
 package help
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/mattermost/mattermost-plugin-apps/apps"
 	"github.com/prokhorind/nextcloud/function/locales"
-	"net/http"
-	"strings"
 	"unicode"
 )
 
@@ -20,38 +17,15 @@ type HelpServiceImpl struct {
 	request apps.CallRequest
 }
 
-func HandleHelpCommand(c *gin.Context) {
-	creq := apps.CallRequest{}
-	json.NewDecoder(c.Request.Body).Decode(&creq)
-	builder := strings.Builder{}
-	builder.WriteString(getSingleHelpMessage(c, creq, "title"))
-	builder.WriteString("\n")
-	if creq.Context.ActingUser.IsSystemAdmin() {
-		builder.WriteString(createHelpForSingleCommand(c, creq, "configure"))
-	}
-	builder.WriteString("\n")
-	builder.WriteString(createHelpForSingleCommand(c, creq, "connect"))
-	builder.WriteString("\n")
-	builder.WriteString(createHelpForSingleCommand(c, creq, "share"))
-	builder.WriteString("\n")
-	builder.WriteString(createHelpForSingleCommand(c, creq, "calendars"))
-	builder.WriteString("\n")
-	builder.WriteString(createHelpForSingleCommand(c, creq, "disconnect"))
-	builder.WriteString("\n")
-	builder.WriteString("\n")
-	builder.WriteString(getSingleHelpMessage(c, creq, "tips"))
-	c.JSON(http.StatusOK, apps.NewTextResponse(builder.String()))
-}
-
-func getSingleHelpMessage(c *gin.Context, request apps.CallRequest, message string) string {
-	locale := request.Context.ActingUser.Locale
-	messageSource := locales.MessageSource{c, locale}
+func (h HelpServiceImpl) getSingleHelpMessage(message string) string {
+	locale := h.request.Context.ActingUser.Locale
+	messageSource := locales.MessageSource{h.c, locale}
 	return messageSource.GetMessage("help." + message)
 }
 
-func createHelpForSingleCommand(c *gin.Context, request apps.CallRequest, command string) string {
-	locale := request.Context.ActingUser.Locale
-	messageSource := locales.MessageSource{c, locale}
+func (h HelpServiceImpl) createHelpForSingleCommand(command string) string {
+	locale := h.request.Context.ActingUser.Locale
+	messageSource := locales.MessageSource{h.c, locale}
 	description := messageSource.GetMessage(fmt.Sprintf("help.%s", command))
 	return fmt.Sprintf("%s - %s", changeFirstLetterToUpperCase(command), description)
 }

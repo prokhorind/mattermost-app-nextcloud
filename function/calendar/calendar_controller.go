d402a0c4be2729cee570effb6ab0897efffbbd4e
omelnychuk <ostapss222@gmail.com>
2023-02-03 00:10:52 +0200

    Calendar controller refactored

--- function/calendar/calendar_controller.go
+++ function/calendar/calendar_controller.go
@@ -8,15 +8,12 @@ import (
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"net/http"
-	"regexp"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mattermost/mattermost-plugin-apps/apps"
 	"github.com/mattermost/mattermost-plugin-apps/apps/appclient"
-	"github.com/mattermost/mattermost-server/v6/model"
 	"github.com/prokhorind/nextcloud/function/oauth"
 )
 
@@ -79,10 +76,13 @@ func DMEventPost(creq apps.CallRequest, calendarService CalendarService, calenda
 		return
 	}
 	vEvent := cal.Events()[0]
-	loc := getMMUserLocation(creq)
+	calendarTimePostService := CalendarTimePostService{}
+
+	loc := calendarTimePostService.GetMMUserLocation(creq)
 
 	postDto := CalendarEventPostDTO{vEvent, asBot, calendar, uuid + ".ics", loc, creq}
-	post := createCalendarEventPost(&postDto)
+	createCalendarEventPostService := CreateCalendarEventPostService{GetMMUser: asBot}
+	post := createCalendarEventPostService.CreateCalendarEventPost(&postDto)
 	post.Message = "Event created"
 	mmUserId := creq.Context.ActingUser.Id
 	asBot.DMPost(mmUserId, post)
@@ -117,11 +117,15 @@ func HandleCreateEventForm(c *gin.Context) {
 	calendarService := CalendarServiceImpl{calendarRequestService: calendarRequestService}
 	option := creq.State.(map[string]interface{})
 
-	loc := getMMUserLocation(creq)
+	calendarTimePostService := CalendarTimePostService{}
+
+	loc := calendarTimePostService.GetMMUserLocation(creq)
+
 	currentUserTime := time.Now().In(loc)
 
 	dateFormatService := DateFormatLocaleService{}
 	parsedLocale := dateFormatService.GetLocaleByTag(creq.Context.ActingUser.Locale)
+	calendarPostServiceImpl := CalendarPostServiceImpl{}
 
 	form := &apps.Form{
 		Title: "Create Nextcloud calendar event",
@@ -153,7 +157,7 @@ func HandleCreateEventForm(c *gin.Context) {
 				Name:                "duration",
 				Label:               "Duration",
 				IsRequired:          true,
-				SelectStaticOptions: prepareMeetingDurations(),
+				SelectStaticOptions: calendarPostServiceImpl.PrepareMeetingDurations(),
 				Value:               apps.SelectOption{Label: "30 minutes", Value: "30 minutes"},
 			},
 			{
@@ -225,7 +229,9 @@ func GetUserSelectedEventsDate(c *gin.Context) {
 	creq := apps.CallRequest{}
 	json.NewDecoder(c.Request.Body).Decode(&creq)
 	calendar := creq.Call.State.(map[string]interface{})["value"].(string)
-	loc := getMMUserLocation(creq)
+	calendarTimePostService := CalendarTimePostService{}
+
+	loc := calendarTimePostService.GetMMUserLocation(creq)
 	currentUserTime := time.Now().In(loc)
 
 	dateFormatService := DateFormatLocaleService{}
@@ -265,7 +271,9 @@ func GetUserSelectedEventsDate(c *gin.Context) {
 func HandleGetEventsToday(c *gin.Context) {
 	creq := apps.CallRequest{}
 	json.NewDecoder(c.Request.Body).Decode(&creq)
-	location := getMMUserLocation(creq)
+	calendarTimePostService := CalendarTimePostService{}
+
+	location := calendarTimePostService.GetMMUserLocation(creq)
 	calendar := creq.Call.State.(map[string]interface{})["value"].(string)
 	HandleGetEvents(c, creq, time.Now().In(location), calendar)
 }
@@ -273,7 +281,9 @@ func HandleGetEventsToday(c *gin.Context) {
 func HandleGetEventsTomorrow(c *gin.Context) {
 	creq := apps.CallRequest{}
 	json.NewDecoder(c.Request.Body).Decode(&creq)
-	location := getMMUserLocation(creq)
+	calendarTimePostService := CalendarTimePostService{}
+
+	location := calendarTimePostService.GetMMUserLocation(creq)
 	calendar := creq.Call.State.(map[string]interface{})["value"].(string)
 	HandleGetEvents(c, creq, time.Now().AddDate(0, 0, 1).In(location), calendar)
 }
@@ -282,25 +292,15 @@ func HandleGetEventsAtSelectedDay(c *gin.Context) {
 	calendar := c.Param("calendar")
 	creq := apps.CallRequest{}
 	json.NewDecoder(c.Request.Body).Decode(&creq)
-	location := getMMUserLocation(creq)
+	calendarTimePostService := CalendarTimePostService{}
+
+	location := calendarTimePostService.GetMMUserLocation(creq)
 	fromDateUTC := creq.Values["from-event-date"].(map[string]interface{})["value"].(string)
 	from, err := time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", fromDateUTC)
 	if err != nil {
 		println(err.Error())
 	}
 	HandleGetEvents(c, creq, from.In(location), calendar)
-}
-
-func getMMUserLocation(creq apps.CallRequest) *time.Location {
-	var timezone string
-	var loc *time.Location
-	if creq.Context.ActingUser.Timezone["useAutomaticTimezone"] == "false" {
-		timezone = creq.Context.ActingUser.Timezone["manualTimezone"]
-	} else {
-		timezone = creq.Context.ActingUser.Timezone["automaticTimezone"]
-	}
-	loc, _ = time.LoadLocation(timezone)
-	return loc
 }
 
 func HandleGetEvents(c *gin.Context, creq apps.CallRequest, date time.Time, calendar string) {
@@ -313,13 +313,14 @@ func HandleGetEvents(c *gin.Context, creq apps.CallRequest, date time.Time, cale
 	remoteUrl := creq.Context.OAuth2.OAuth2App.RemoteRootURL
 	userId := creq.Context.OAuth2.User.(map[string]interface{})["user_id"].(string)
 	reqUrl := fmt.Sprintf("%s/remote.php/dav/calendars/%s/%s", remoteUrl, userId, calendar)
+	calendarTimePostService := CalendarTimePostService{}
 
-	loc := getMMUserLocation(creq)
+	loc := calendarTimePostService.GetMMUserLocation(creq)
 
 	asBot := appclient.AsBot(creq.Context)
 	mmUserId := creq.Context.ActingUser.Id
 
-	from, to := prepareTimeRangeForGetEventsRequest(date)
+	from, to := calendarTimePostService.PrepareTimeRangeForGetEventsRequest(date)
 	eventRange := CalendarEventRequestRange{
 		From: from,
 		To:   to,
@@ -355,313 +356,14 @@ func HandleGetEvents(c *gin.Context, creq apps.CallRequest, date time.Time, cale
 		return
 	}
 
+	calendarPostServiceImpl := CreateCalendarEventPostService{GetMMUser: asBot}
 	for i, e := range dailyCalendarEvents {
 		postDto := CalendarEventPostDTO{&e, asBot, calendar, eventIds[i], loc, creq}
-		post := createCalendarEventPost(&postDto)
+		post := calendarPostServiceImpl.CreateCalendarEventPost(&postDto)
 		asBot.DMPost(mmUserId, post)
 	}
 
 	c.JSON(http.StatusOK, apps.NewDataResponse(nil))
-}
-
-func prepareTimeRangeForGetEventsRequest(chosenDate time.Time) (time.Time, time.Time) {
-	date := chosenDate.Add(-time.Minute * time.Duration(chosenDate.Minute()))
-	date = date.Add(-time.Hour * time.Duration(chosenDate.Hour()))
-	date = date.Add(-time.Second * time.Duration(chosenDate.Second()))
-	return date.AddDate(0, 0, -1), date.AddDate(0, 0, 1)
-}
-
-func createCalendarEventPost(postDTO *CalendarEventPostDTO) *model.Post {
-	var name, organizer, eventStatus string
-	for _, e := range postDTO.event.Properties {
-		if e.BaseProperty.IANAToken == "ORGANIZER" {
-			organizer = e.BaseProperty.Value
-		}
-		if e.BaseProperty.IANAToken == "SUMMARY" {
-			name = e.BaseProperty.Value
-		}
-		if e.BaseProperty.IANAToken == "STATUS" {
-			eventStatus = e.BaseProperty.Value
-		}
-	}
-
-	userId := postDTO.creq.Context.OAuth2.User.(map[string]interface{})["user_id"].(string)
-	remoteUrl := postDTO.creq.Context.OAuth2.OAuth2App.RemoteRootURL
-	reqUrl := fmt.Sprintf("%s/remote.php/dav/calendars/%s/%s/%s", remoteUrl, userId, postDTO.calendarId, postDTO.eventId)
-
-	post := model.Post{}
-	commandBinding := apps.Binding{
-		Location:    "embedded",
-		AppID:       "nextcloud",
-		Label:       createNameForEvent(name, postDTO),
-		Description: "Going?",
-		Bindings:    []apps.Binding{},
-	}
-	calendarService := CalendarServiceImpl{}
-
-	if eventStatus == "CANCELLED" {
-		commandBinding.Label = fmt.Sprintf("Cancelled ~~%s~~", commandBinding.Label)
-		commandBinding.Description = fmt.Sprintf("~~%s~~", commandBinding.Description)
-		m1 := make(map[string]interface{})
-		m1["app_bindings"] = []apps.Binding{commandBinding}
-
-		post.SetProps(m1)
-
-		return &post
-	}
-
-	if strings.Contains(organizer, ":") {
-		organizer = strings.Split(organizer, ":")[1]
-	}
-	organizerEmail := postDTO.creq.Context.ActingUser.Email
-	status := FindAttendeeStatus(postDTO.bot, *postDTO.event, postDTO.creq.Context.ActingUser.Id)
-
-	if organizerEmail != organizer {
-		path := fmt.Sprintf("/users/%s/calendars/%s/events/%s/status", userId, postDTO.calendarId, postDTO.eventId)
-		commandBinding = calendarService.AddButtonsToEvents(commandBinding, string(status), path)
-	}
-
-	deletePath := fmt.Sprintf("/delete-event/%s/events/%s", postDTO.calendarId, postDTO.eventId)
-	сreateViewButton(&commandBinding, "view-details", organizer, "View Details", postDTO, name, reqUrl)
-	сreateDeleteButton(&commandBinding, "Delete", "Delete", deletePath)
-	m1 := make(map[string]interface{})
-	m1["app_bindings"] = []apps.Binding{commandBinding}
-
-	post.SetProps(m1)
-
-	return &post
-}
-
-func createMeetingStartButton(commandBinding *apps.Binding, link string, location apps.Location) {
-	commandBinding.Bindings = append(commandBinding.Bindings, apps.Binding{
-		Location: location,
-		Label:    fmt.Sprintf("Join %s Meeting", location),
-		Submit:   apps.NewCall("/redirect/meeting").WithState(link),
-	})
-}
-
-func сastUserEmailsToMMUserNicknames(attendees []*ics.Attendee, bot appclient.Client) string {
-	var attendeesNicknames string
-	for _, attendee := range attendees {
-		attendeesNicknames += сastSingleEmailToMMUserNickname(attendee.Email(), attendee.ICalParameters["PARTSTAT"][0], bot)
-	}
-	if len(attendeesNicknames) != 0 {
-		attendeesNicknames = attendeesNicknames[:len(attendeesNicknames)-1]
-	}
-	return attendeesNicknames
-}
-
-func сastSingleEmailToMMUserNickname(email string, status string, bot appclient.Client) string {
-	if strings.Contains(email, ":") {
-		email = strings.Split(email, ":")[1]
-	}
-	mmUser, _, err := bot.GetUserByEmail(email, "")
-	if err == nil {
-		if status == "" {
-			return "@" + mmUser.Username + "-" + email + " "
-		}
-		return "@" + mmUser.Username + "-" + email + "-" + status + " "
-	} else {
-		return email + "-" + status + " "
-	}
-}
-
-func createDateForEventInForm(postDTO *CalendarEventPostDTO) string {
-	locale := postDTO.creq.Context.ActingUser.Locale
-	dateFormatService := DateFormatLocaleService{}
-	parsedLocale := dateFormatService.GetLocaleByTag(locale)
-	start, _ := postDTO.event.GetStartAt()
-	finish, _ := postDTO.event.GetEndAt()
-
-	format := dateFormatService.GetTimeFormatsByLocale(parsedLocale)
-	dayFormat := dateFormatService.GetFullFormatsByLocale(parsedLocale)
-	day := strconv.Itoa(start.Day())
-	month := strconv.Itoa(int(start.Month()))
-	if len(day) < 2 {
-		day = "0" + day
-	}
-	if len(month) < 2 {
-		month = "0" + month
-	}
-
-	return fmt.Sprintf("%s %s-%s", start.In(postDTO.loc).Format(dayFormat), start.In(postDTO.loc).Format(format), finish.In(postDTO.loc).Format(format))
-}
-
-func createNameForEvent(name string, postDTO *CalendarEventPostDTO) string {
-	locale := postDTO.creq.Context.ActingUser.Locale
-	dateFormatService := DateFormatLocaleService{}
-	parsedLocale := dateFormatService.GetLocaleByTag(locale)
-	start, _ := postDTO.event.GetStartAt()
-	finish, _ := postDTO.event.GetEndAt()
-
-	format := dateFormatService.GetTimeFormatsByLocale(parsedLocale)
-	dayFormat := dateFormatService.GetFullFormatsByLocale(parsedLocale)
-	day := strconv.Itoa(start.Day())
-	month := strconv.Itoa(int(start.Month()))
-	if len(day) < 2 {
-		day = "0" + day
-	}
-	if len(month) < 2 {
-		month = "0" + month
-	}
-	remoteUrl := postDTO.creq.Context.OAuth2.RemoteRootURL
-	calendarUrl := fmt.Sprintf("%s%s%s-%s-%s", remoteUrl, "/apps/calendar/timeGridDay/", strconv.Itoa(start.Year()), month, day)
-	return fmt.Sprintf("[%s](%s) %s %s-%s", name, calendarUrl, start.In(postDTO.loc).Format(dayFormat), start.In(postDTO.loc).Format(format), finish.In(postDTO.loc).Format(format))
-}
-
-func prepareMeetingDurations() []apps.SelectOption {
-	var durations []apps.SelectOption
-	durations = append(durations, apps.SelectOption{
-		Label: "15 minutes",
-		Value: "15 minutes",
-	})
-	durations = append(durations, apps.SelectOption{
-		Label: "30 minutes",
-		Value: "30 minutes",
-	})
-	durations = append(durations, apps.SelectOption{
-		Label: "45 minutes",
-		Value: "45 minutes",
-	})
-	durations = append(durations, apps.SelectOption{
-		Label: "1 hour",
-		Value: "1 hour",
-	})
-	durations = append(durations, apps.SelectOption{
-		Label: "1.5 hours",
-		Value: "1.5 hours",
-	})
-	durations = append(durations, apps.SelectOption{
-		Label: "2 hours",
-		Value: "2 hours",
-	})
-	durations = append(durations, apps.SelectOption{
-		Label: "All day",
-		Value: "All day",
-	})
-	return durations
-}
-
-func сreateDeleteButton(commandBinding *apps.Binding, location apps.Location, label string, deletePath string) {
-	expand := apps.Expand{
-		OAuth2App:             apps.ExpandAll,
-		OAuth2User:            apps.ExpandAll,
-		ActingUserAccessToken: apps.ExpandAll,
-		ActingUser:            apps.ExpandAll,
-	}
-	commandBinding.Bindings = append(commandBinding.Bindings, apps.Binding{
-		Location: location,
-		Label:    label,
-		Submit:   apps.NewCall(deletePath).WithExpand(expand),
-	})
-}
-
-func сreateViewButton(commandBinding *apps.Binding, location apps.Location, organizer string, label string, postDTO *CalendarEventPostDTO, name string, reqUrl string) {
-	event := postDTO.event
-	bot := postDTO.bot
-	property := event.GetProperty(ics.ComponentPropertyDescription)
-	var description string
-	if property == nil {
-		description = ""
-	} else {
-		description = property.Value
-	}
-	zoomLinks, googleMeetLinks := getZoomAndGoogleMeetLinksFromDescription(description)
-
-	commandBinding.Bindings = append(commandBinding.Bindings, apps.Binding{
-		Location: location,
-		Label:    label,
-		Form: &apps.Form{
-			Title: name,
-			Fields: []apps.Field{
-				{
-					Type:       apps.FieldTypeText,
-					Name:       "Date",
-					Label:      "Date",
-					ReadOnly:   true,
-					Value:      createDateForEventInForm(postDTO),
-					IsRequired: true,
-				},
-				{
-					Type:        apps.FieldTypeText,
-					Name:        "Description",
-					Label:       "Description",
-					ReadOnly:    true,
-					Value:       description,
-					TextSubtype: apps.TextFieldSubtypeTextarea,
-				},
-				{
-					Type:                apps.FieldTypeStaticSelect,
-					Name:                "Attendees",
-					Label:               "Attendees",
-					SelectIsMulti:       true,
-					Value:               prepareAttendeeStaticSelect(сastUserEmailsToMMUserNicknames(event.Attendees(), *bot)),
-					SelectStaticOptions: prepareAttendeeStaticSelect(сastUserEmailsToMMUserNicknames(event.Attendees(), *bot)),
-					ReadOnly:            true,
-				},
-				{
-					Type:       apps.FieldTypeText,
-					Name:       "Organizer",
-					Label:      "Organizer",
-					ReadOnly:   true,
-					IsRequired: true,
-					Value:      сastSingleEmailToMMUserNickname(organizer, "", *bot),
-				},
-			},
-			Submit: apps.NewCall("/do-nothing"),
-		},
-	})
-	i := len(commandBinding.Bindings) - 1
-	if len(zoomLinks) != 0 {
-		commandBinding.Bindings[i].Form.Fields = append(commandBinding.Bindings[i].Form.Fields, apps.Field{
-			Type:        apps.FieldTypeText,
-			Name:        "ZoomUrl",
-			Label:       "ZoomLink",
-			Value:       zoomLinks,
-			ReadOnly:    true,
-			IsRequired:  true,
-			TextSubtype: apps.TextFieldSubtypeURL,
-		})
-		createMeetingStartButton(commandBinding, strings.Split(zoomLinks, " ")[0], "Zoom")
-	}
-	if len(googleMeetLinks) != 0 {
-		commandBinding.Bindings[i].Form.Fields = append(commandBinding.Bindings[i].Form.Fields, apps.Field{
-			Type:        apps.FieldTypeText,
-			Name:        "GoogleMeetUrl",
-			Label:       "Google-Meet-Link",
-			Value:       googleMeetLinks,
-			ReadOnly:    true,
-			IsRequired:  true,
-			TextSubtype: apps.TextFieldSubtypeURL,
-		})
-		createMeetingStartButton(commandBinding, strings.Split(googleMeetLinks, " ")[0], "Google Meet")
-	}
-	commandBinding.Bindings[i].Form.Fields = append(commandBinding.Bindings[i].Form.Fields, apps.Field{
-		Type:        apps.FieldTypeText,
-		Name:        "Event-Import",
-		Label:       "Event-Import",
-		Description: "Use this link to import event in your calendars",
-		Value:       reqUrl,
-		ReadOnly:    true,
-		IsRequired:  true,
-		TextSubtype: apps.TextFieldSubtypeURL,
-	})
-}
-
-func getZoomAndGoogleMeetLinksFromDescription(description string) (string, string) {
-	zoomPattern := regexp.MustCompile(`https:\/\/[\w-]*\.?zoom.us\/(j|my)\/[\d\w?=-]+`)
-	googleMeetPattern := regexp.MustCompile(`https?:\/\/(.+?\.)?meet\.google\.com(\/[A-Za-z0-9\-]*)?`)
-	zoomLinks := zoomPattern.FindAllString(description, -1)
-	googleMeetLinks := googleMeetPattern.FindAllString(description, -1)
-	return strings.Join(zoomLinks, " "), strings.Join(googleMeetLinks, " ")
-}
-
-func prepareAttendeeStaticSelect(attendees string) []apps.SelectOption {
-	options := make([]apps.SelectOption, 0)
-	for _, a := range strings.Split(attendees, " ") {
-		options = append(options, apps.SelectOption{Label: a, Value: a})
-	}
-	return options
 }
 
 func HandleChangeEventStatus(c *gin.Context) {
@@ -757,59 +459,12 @@ func HandleGetUserCalendars(c *gin.Context) {
 
 	asBot := appclient.AsBot(creq.Context)
 
+	calendarPostServiceImpl := CalendarPostServiceImpl{}
+
 	for _, c := range userCalendars {
-		post := createCalendarPost(c)
+		post := calendarPostServiceImpl.CreateCalendarPost(c)
 		asBot.DMPost(creq.Context.ActingUser.Id, post)
 	}
 	c.JSON(http.StatusOK, apps.NewTextResponse(""))
 
-}
-
-func createCalendarPost(option apps.SelectOption) *model.Post {
-	post := model.Post{}
-	commandBinding := apps.Binding{
-		Location:    "embedded",
-		AppID:       "nextcloud",
-		Label:       "Calendar " + option.Label,
-		Description: "Calendar actions",
-		Bindings:    []apps.Binding{},
-	}
-
-	createGetCalendarEventsButton(&commandBinding, option, "Calendar", "Today", "today")
-	createGetCalendarEventsButton(&commandBinding, option, "Calendar", "Tomorrow", "tomorrow")
-	createGetCalendarEventsButton(&commandBinding, option, "Calendar", "Select date", "select-date-form")
-	createCalendarEventsButton(&commandBinding, option, "Calendar", "Create event")
-
-	m1 := make(map[string]interface{})
-	m1["app_bindings"] = []apps.Binding{commandBinding}
-
-	post.SetProps(m1)
-	return &post
-
-}
-
-func createGetCalendarEventsButton(commandBinding *apps.Binding, option apps.SelectOption, location apps.Location, label string, day string) {
-	commandBinding.Bindings = append(commandBinding.Bindings, apps.Binding{
-		Location: location,
-		Label:    label,
-		Submit: apps.NewCall("/get-calendar-events-" + day).WithExpand(apps.Expand{
-			OAuth2App:             apps.ExpandAll,
-			OAuth2User:            apps.ExpandAll,
-			ActingUserAccessToken: apps.ExpandAll,
-			ActingUser:            apps.ExpandAll,
-		}).WithState(option),
-	})
-}
-
-func createCalendarEventsButton(commandBinding *apps.Binding, option apps.SelectOption, location apps.Location, label string) {
-	commandBinding.Bindings = append(commandBinding.Bindings, apps.Binding{
-		Location: location,
-		Label:    label,
-		Submit: apps.NewCall("/create-calendar-event-form").WithExpand(apps.Expand{
-			OAuth2App:             apps.ExpandAll,
-			OAuth2User:            apps.ExpandAll,
-			ActingUserAccessToken: apps.ExpandAll,
-			ActingUser:            apps.ExpandAll,
-		}).WithState(option),
-	})
 }

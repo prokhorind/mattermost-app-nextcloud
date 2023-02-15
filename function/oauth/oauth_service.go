e7aae219aa930aaa02b4d147467fbacb0d4f30dc
omelnychuk <ostapss222@gmail.com>
2023-02-15 13:29:31 +0200

    http clients replaced with retryable http client. Logs added

--- function/oauth/oauth_service.go
+++ function/oauth/oauth_service.go
@@ -4,7 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/hashicorp/go-retryablehttp"
+	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/mattermost/mattermost-plugin-apps/apps"
 )
@@ -36,10 +40,16 @@ func (s OauthServiceImpl) RefreshToken() Token {
 	req, _ := http.NewRequest("POST", reqUrl, bytes.NewBuffer(body))
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	req.SetBasicAuth(clientId, clientSecret)
+	maxRetries, _ := strconv.Atoi(os.Getenv("MAX_REQUEST_RETRIES"))
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = maxRetries
 
-	client := &http.Client{}
-	resp, _ := client.Do(req)
+	client := retryClient.StandardClient()
+	resp, err := client.Do(req)
 	defer resp.Body.Close()
+	if err != nil {
+		log.Errorf("Error during refreshing of the token. Error: %s", err)
+	}
 
 	jsonResp := Token{}
 	json.NewDecoder(resp.Body).Decode(&jsonResp)
@@ -68,9 +78,16 @@ func getToken(creq apps.CallRequest) Token {
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	req.SetBasicAuth(clientId, clientSecret)
 
-	client := &http.Client{}
-	resp, _ := client.Do(req)
+	maxRetries, _ := strconv.Atoi(os.Getenv("MAX_REQUEST_RETRIES"))
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = maxRetries
+
+	client := retryClient.StandardClient()
+	resp, err := client.Do(req)
 	defer resp.Body.Close()
+	if err != nil {
+		log.Errorf("Error during getting of the token. Error: %s", err)
+	}
 	jsonResp := Token{}
 	json.NewDecoder(resp.Body).Decode(&jsonResp)
 	return jsonResp
